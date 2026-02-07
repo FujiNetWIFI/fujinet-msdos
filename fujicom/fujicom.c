f51744c79fa195e67fdeda537d9b807940dfc78d
Chris Osborn <fozztexx@fozztexx.com>
2026-02-07 10:25:01 -0800

    Allow specifying FUJI_PORT as FUJI_PORT=0x123,12 or FUJI_PORT=123h,12

--- fujicom/fujicom.c
+++ fujicom/fujicom.c
@@ -77,39 +77,58 @@ struct SREGS f5status;
 
 void fujicom_init(void)
 {
-  int base, irq;
-  int divisor;
-  long bps = SERIAL_BPS;
+  unsigned base, irq;
+  unsigned divisor;
+  const char *fuji_port, *comma;
+  unsigned port_len;
+  unsigned long bps = SERIAL_BPS;
   int comp = 1;
 
 
   // FIXME - allow FUJI_PORT to be "<base>,<irq>" in addition to 1-4
-  if (getenv("FUJI_PORT"))
-    comp = atoi(getenv("FUJI_PORT"));
   if (getenv("FUJI_BPS"))
-    bps = atol(getenv("FUJI_BPS"));
+    bps = strtoul(getenv("FUJI_BPS"), NULL, 10);
 
-  switch (comp) {
-  default:
-  case 1:
-    base = COM1_UART;
-    irq = COM1_INTERRUPT;
-    break;
-  case 2:
-    base = COM2_UART;
-    irq = COM2_INTERRUPT;
-    break;
-  case 3:
-    base = COM3_UART;
-    irq = COM3_INTERRUPT;
-    break;
-  case 4:
-    base = COM4_UART;
-    irq = COM4_INTERRUPT;
-    break;
+  fuji_port = getenv("FUJI_PORT");
+  if (fuji_port) {
+    comma = strchr(fuji_port, ',');
+    if (comma)
+      port_len = comma - fuji_port;
+    else
+      port_len = strlen(fuji_port);
+
+    if (!strcasecmp(fuji_port, "0x", 2))
+      base = strtoul(fuji_port + 2, NULL, 16);
+    else if (tolower(fuji_port[port_len - 1]) == 'h')
+      base = strtoul(fuji_port, NULL, 16);
+    else {
+      comp = atoi(fuji_port);
+      switch (comp) {
+      default:
+      case 1:
+        base = COM1_UART;
+        irq = COM1_INTERRUPT;
+        break;
+      case 2:
+        base = COM2_UART;
+        irq = COM2_INTERRUPT;
+        break;
+      case 3:
+        base = COM3_UART;
+        irq = COM3_INTERRUPT;
+        break;
+      case 4:
+        base = COM4_UART;
+        irq = COM4_INTERRUPT;
+        break;
+      }
+    }
+
+    if (comma)
+      irq = atoi(comma + 1);
   }
 
-  divisor = 115200L / bps;
+  divisor = 115200UL / bps;
   port_init(base, divisor);
 #if defined(DEBUG) || defined(INIT_INFO)
   consolef("Port: %xh  BPS: %ld/%d\n", port_uart_base, (int32_t) bps, divisor);
